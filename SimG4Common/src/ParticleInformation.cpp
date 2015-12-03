4f642b914b6c0e5ded7b615169ce0e96104a8bcd
Anna <anna.zaborowska@cern.ch>
2015-12-03 11:17:04 +0100

    GeantSim moved completely to Sim/

---
+++ SimG4Common/src/ParticleInformation.cpp
@@ -0,0 +1,15 @@
+#include "SimG4Common/ParticleInformation.h"
+
+ParticleInformation::ParticleInformation(const MCParticleHandle& aMCpart, ParticleHandle& aPart): m_mcpart(aMCpart), m_part(aPart) {}
+
+ParticleInformation::~ParticleInformation() {}
+
+void ParticleInformation::Print() const {}
+
+const MCParticleHandle& ParticleInformation::GetMCParticleHandle() const {
+   return m_mcpart;
+}
+
+ParticleHandle& ParticleInformation::GetParticleHandle() {
+   return m_part;
+}
