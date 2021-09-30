1e5331c73bbbc31565229750cd0c3cc7488f0832
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-09-30 17:43:56 +0200

    add cellid only for current collection

--- SimG4Components/src/SimG4SaveTrackerHits.cpp
+++ SimG4Components/src/SimG4SaveTrackerHits.cpp
@@ -70,16 +70,16 @@ StatusCode SimG4SaveTrackerHits::saveOutput(const G4Event& aEvent) {
     edm4hep::SimTrackerHitCollection* edmHits = m_trackHits.createAndPut();
     for (int iter_coll = 0; iter_coll < collections->GetNumberOfCollections(); iter_coll++) {
       collect = collections->GetHC(iter_coll);
-
-      // Add CellID encoding string to collection metadata
-      auto lcdd = m_geoSvc->lcdd();
-      auto allReadouts = lcdd->readouts();
-      auto idspec = lcdd->idSpecification(collect->GetName());
-      auto field_str = idspec.fieldDescription();
-      auto& coll_md = m_podioDataSvc->getProvider().getCollectionMetaData( m_trackHits.get()->getID() );
-      coll_md.setValue("CellIDEncodingString", field_str);
-
       if (std::find(m_readoutNames.begin(), m_readoutNames.end(), collect->GetName()) != m_readoutNames.end()) {
+
+        // Add CellID encoding string to collection metadata
+        auto lcdd = m_geoSvc->lcdd();
+        auto allReadouts = lcdd->readouts();
+        auto idspec = lcdd->idSpecification(collect->GetName());
+        auto field_str = idspec.fieldDescription();
+        auto& coll_md = m_podioDataSvc->getProvider().getCollectionMetaData( m_trackHits.get()->getID() );
+        coll_md.setValue("CellIDEncodingString", field_str);
+
         size_t n_hit = collect->GetSize();
         verbose() << "\t" << n_hit << " hits are stored in a tracker collection #" << iter_coll << ": "
                << collect->GetName() << endmsg;
