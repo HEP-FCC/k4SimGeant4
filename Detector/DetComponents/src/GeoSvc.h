8ac4e813d62247dea140ac83922087e3a587bab6
Juraj Smiesko <34742917+kjvbrt@users.noreply.github.com>
2024-02-08 21:58:14 +0100

    Revert "Remove the deprecated lcdd() (#58)" (#65)
    This reverts commit 5c3826dc5bdffe3310d5b9d4e83001d75b74db74.

--- Detector/DetComponents/src/GeoSvc.h
+++ Detector/DetComponents/src/GeoSvc.h
@@ -37,6 +37,8 @@ public:
   StatusCode buildGeant4Geo();
   // receive DD4hep Geometry
   virtual dd4hep::DetElement getDD4HepGeo() override;
+  [[deprecated("Use getDetector() instead")]]
+  virtual dd4hep::Detector* lcdd() override;
   virtual dd4hep::Detector* getDetector() override;
   virtual std::string constantAsString(std::string const& name) override;
   // receive Geant4 Geometry
